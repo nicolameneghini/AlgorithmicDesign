7b3d40d1055e33b5e724ffe2bad55a6caa6a8690
Nicola Meneghini <nicolameneghini@hotmail.it>
2019-05-28 11:25:16 +0200

    added comments

--- exercises/01strassen/strassen.c
+++ exercises/01strassen/strassen.c
@@ -69,6 +69,9 @@ void naive_aux(float **C, const size_t C_f_row, const size_t C_f_col,
   }
 }
 
+//The idea is to allocate less memory by reusing the space of S and P. So, instead of allocating n2*n2*10 and n2*n2*7,
+//I only allocate n2*n2*2 and n2*n2*3
+
 void strassen_aux_improved(float **C, const size_t C_f_row, const size_t C_f_col,
                            float **A, const size_t A_f_row, const size_t A_f_col,
                            float **B, const size_t B_f_row, const size_t B_f_col,
@@ -134,16 +137,11 @@ void strassen_aux_improved(float **C, const size_t C_f_row, const size_t C_f_col
                         B, B2X, BX2,
                         n2);
 
-  // Now that I got P1 and P2 a I can compute C12
   // C12 = P1 + P2
   sum_matrix_blocks(C, C1X, CX2,
                     P[0], 0, 0,
                     P[1], 0, 0,
                     n2);
-
-  //Since i will not need P1 and P2 anymore i can overwrite that data region
-
-  //Now i will compute P3 and P4 in order to compute C21
 
   // S3 = A21 + A22
   sum_matrix_blocks(S[0], 0, 0,
@@ -156,7 +154,6 @@ void strassen_aux_improved(float **C, const size_t C_f_row, const size_t C_f_col
                         S[0], 0, 0,
                         B, B1X, BX1,
                         n2);
-  // Now that I have P3 I can partially compute C22 as P1 - P3 so that i can free P1 which will be no longer needed afterwards
   sub_matrix_blocks(C, C2X, CX2,
                     P[0], 0, 0,
                     P[2], 0, 0,
@@ -178,12 +175,10 @@ void strassen_aux_improved(float **C, const size_t C_f_row, const size_t C_f_col
                     P[2], 0, 0,
                     n2);
 
-  // Before going on I partially compute C21 but P4 -P2 so that i can free this regions
   sub_matrix_blocks(C, C1X, CX1,
                     P[0], 0, 0,
                     P[1], 0, 0,
                     n2);
-  // Ok now I have all the three P matrices free and both of the S matrices free
   // S5 = A11 + A22
   sum_matrix_blocks(S[0], 0, 0,
                     A, A1X, AX1,
@@ -201,7 +196,6 @@ void strassen_aux_improved(float **C, const size_t C_f_row, const size_t C_f_col
                         S[0], 0, 0,
                         S[1], 0, 0,
                         n2);
-  // Now I can add P5 both in C22 and C11 in order to free the memory space
 
   sum_matrix_blocks(C, C1X, CX1,
                     C, C1X, CX1,
@@ -212,7 +206,6 @@ void strassen_aux_improved(float **C, const size_t C_f_row, const size_t C_f_col
                     C, C2X, CX2,
                     P[0], 0, 0,
                     n2);
-  // now P5 will be no longer used and we can use P[0]
 
   // S7 = A12 - A22 (there was a bug here A21 - A22)
   sub_matrix_blocks(S[0], 0, 0,
@@ -230,13 +223,10 @@ void strassen_aux_improved(float **C, const size_t C_f_row, const size_t C_f_col
                         S[0], 0, 0,
                         S[1], 0, 0,
                         n2);
-  // we then use P6
   sum_matrix_blocks(C, C1X, CX1,
                     C, C1X, CX1,
                     P[0], 0, 0,
                     n2);
-
-  // and finally
 
   // S9 = A11 - A21
   sub_matrix_blocks(S[0], 0, 0,
@@ -260,9 +250,6 @@ void strassen_aux_improved(float **C, const size_t C_f_row, const size_t C_f_col
                     C, C2X, CX2,
                     P[0], 0, 0,
                     n2);
-  // C11 = (P5 + P4) - P2 + P6
-
-  // C22 = P5 + P1 - P3 - P7
 
   for (int i = 0; i < 2; i++)
   {
